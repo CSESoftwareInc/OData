accd1e21cb917bb40be2e6243a64e68fa2291162
Cameron Anderson <canderson3.14@me.com>
2020-10-20 16:29:43 -0500

    .NET Framework fixes (#6)
    Added .NET Framework tests and fixed issue where .NET Framework expressions may create invalid parentheses

---
+++ src/CSESoftware.OData.FrameworkTests/Helpers/Employee.cs
@@ -0,0 +1,9 @@
+﻿using CSESoftware.Core.Entity;
+
+namespace CSESoftware.OData.FrameworkTests.Helpers
+{
+    public class Employee : BaseEntity<int>
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ src/CSESoftware.OData.FrameworkTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CSESoftware.OData.FrameworkTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CSESoftware.OData.FrameworkTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f9c562f0-6195-4997-b423-a7b373b28eb2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
